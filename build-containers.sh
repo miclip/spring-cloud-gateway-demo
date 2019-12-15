92aa49abb4e9233b962b34ff0fb7d8b89951ead2
Michael Lipscombe <mlipscombe@outlook.com>
2019-12-15 17:52:27 +0900

    Added Hystrix Dashboard

--- build-containers.sh
+++ build-containers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=0.0.11
+VERSION=0.0.18
 
 # Update all Kubernetes Deployments to use latest tag
 sed -ir "s/\(miclip\/[A-Za-z]*-service:\)\(.*\)/\1${VERSION}/g" ./spring-cloud-gateway/*/manifest.yml
@@ -36,6 +36,16 @@ pushd ./spring-cloud-gateway/$SERVICE_NAME > /dev/null
 popd > /dev/null
 
 SERVICE_NAME=gateway-service
+pushd ./spring-cloud-gateway/$SERVICE_NAME > /dev/null
+    JAR_NAME=./target/$SERVICE_NAME-0.0.1-SNAPSHOT.jar
+    IMAGE_NAME=miclip/$SERVICE_NAME:$VERSION
+    $MAVEN_HOME/bin/mvn -Dmaven.test.failure.ignore=true clean install
+    docker build -t $IMAGE_NAME . --build-arg JAR_FILE=$JAR_NAME
+    docker push $IMAGE_NAME
+    kubectl apply -f ./manifest.yml
+popd > /dev/null
+
+SERVICE_NAME=hystrix-dashboard
 pushd ./spring-cloud-gateway/$SERVICE_NAME > /dev/null
     JAR_NAME=./target/$SERVICE_NAME-0.0.1-SNAPSHOT.jar
     IMAGE_NAME=miclip/$SERVICE_NAME:$VERSION
